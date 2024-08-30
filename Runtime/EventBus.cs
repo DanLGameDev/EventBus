e8bb978195202fdd890f481c4b645f3e482ae5b4
Daniel Loughmiller <dan@effectwebagency.com>
2024-08-30 15:39:42 -0400

    added documentation to eventbus class

--- Runtime/EventBus.cs
+++ Runtime/EventBus.cs
@@ -7,26 +7,50 @@ namespace DGP.EventBus.Runtime
     public static class EventBus<T> where T : IEvent
     {
         internal static readonly HashSet<EventBinding<T>> Bindings = new();
-        private static readonly List<EventBinding<T>> bindingsPendingRemoval = new();
+        private static readonly List<EventBinding<T>> BindingsPendingRemoval = new();
       
         // ReSharper disable once StaticMemberInGenericType
         private static bool _isCurrentlyRaising;
 
+        /// <summary>
+        /// Registers an EventBinding to the EventBus
+        /// </summary>
+        /// <param name="binding">The event binding to register</param>
         public static EventBinding<T> Register(EventBinding<T> binding) {
             Bindings.Add(binding);
             return binding;
         }
+        
+        /// <summary>
+        /// Registers an action of a given event type to the EventBus and returns the binding
+        /// </summary>
+        /// <param name="onEvent">The Action<T> to invoke when the event occurs</param>
+        /// <returns>The event binding created by this method</returns>
         public static EventBinding<T> Register(Action<T> onEvent) => Register(new EventBinding<T>(onEvent));
+        
+        /// <summary>
+        /// Registers an action with no arguments to the EventBus and returns the binding
+        /// </summary>
+        /// <param name="onEventNoArgs">The Action to invoke when the event occurs</param>
+        /// <returns>The event binding created by this method</returns>
         public static EventBinding<T> Register(Action onEventNoArgs) => Register(new EventBinding<T>(onEventNoArgs));
         
+        /// <summary>
+        /// De-registers an event binding from the EventBus
+        /// </summary>
+        /// <param name="binding">The binding to deregister</param>
         public static void Deregister(EventBinding<T> binding) {
             if (_isCurrentlyRaising) {
-                bindingsPendingRemoval.Add(binding);
+                BindingsPendingRemoval.Add(binding);
                 return;
             }
             Bindings.Remove(binding);
         }
         
+        /// <summary>
+        /// Raises the event, invoking all registered event bindings
+        /// </summary>
+        /// <param name="event">The event to invoke</param>
         public static void Raise(T @event = default(T))
         {
             #if UNITY_EDITOR && ODIN_INSPECTOR
@@ -47,7 +71,7 @@ namespace DGP.EventBus.Runtime
         }
         
         private static void ProcessPendingRemovals() {
-            foreach (var binding in bindingsPendingRemoval) {
+            foreach (var binding in BindingsPendingRemoval) {
                 Bindings.Remove(binding);
             }
         }
